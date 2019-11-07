c1c60de4bb11378e1a7657bf4cdde35425bc5c8f
bruno.buzzi <bruno.brasesco@gmail.com>
2019-11-07 11:46:25 -0300

    update register-application.sh for #239

--- scripts/register-application.sh
+++ scripts/register-application.sh
@@ -1,8 +1,20 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
-$GS_HOME/bin/startTopaz devKit_33 -il <<EOF >>MFC.out
-set user DataCurator password swordfish gemstone devKit_33
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage: register-application (with no arguments)"
+  echo "This script is to register Bpm Flow web application and it should be executed only once after the installation process";
+  echo "To unregister the web application see unregister-application script"     
+  echo "The environment variable GS_HOME must be set";
+  echo "This script is used in conjunction with unregister-application script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
+nohup $GS_HOME/bin/startTopaz devKit_34 -il <<EOF >>MFC.out
+set user DataCurator password swordfish gemstone devKit_34
 login
 exec 
 System beginTransaction.
