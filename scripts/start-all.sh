6e5c60db3b49db8182a4cb47cfc33b083d966b64
bruno.buzzi <bruno.brasesco@gmail.com>
2019-11-07 11:41:20 -0300

    update start-all.sh for #239

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,20 +1,45 @@
 #Requires GS_HOME variable defined
 #! /bin/sh
 #set -x
-$GS_HOME/bin/startTopaz devKit_33 -il <<EOF >>MFC.out
-set user DataCurator password swordfish gemstone devKit_33
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage: start-all (with no arguments)"
+  echo "Start all Web Servers contained in the file (ports-all.ini):"; 
+  if [ ! -f ports-all.ini ]; 
+    then echo "ports-all.ini file does not exist the script can not be executed"
+    else (cat ports-all.ini; echo)
+  fi
+  echo "The environment variable GS_HOME must be set";
+  echo "The (ports-all.ini) file has to have the following format: port1,port2,port3";
+  echo "For example: 8787,8888,8989";  
+  echo "This script is used in conjunction with stop-all script";   
+  exit 0
+fi
+if [ -z ${GS_HOME+x} ]; then
+  echo "GS_HOME variable is unset. Set this variable first and try again...";
+  exit 0
+fi
+nohup $GS_HOME/bin/startTopaz devKit_34 -u "WebServer" -il <<EOF >>MFC.out
+set user DataCurator password swordfish gemstone devKit_34
 login
 exec 
-   | handler commitThreshold |
-
-   commitThreshold := 65.
-   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | self halt ].
+   | handler commitThreshold usedMemory |
+   commitThreshold := 80.
+   handler := AlmostOutOfMemory addDefaultHandler: [ :ex | 
+     Transcript show: ('AlmostOutOfMemory: ', ex printString); cr.
+     System commitTransaction ifFalse: [Transcript show: 'AutoCommit failed'. nil error: 'AutoCommit failed' ].
+     System _vmMarkSweep. "GC executed to remove unnecessary objects"
+     Transcript show: ('GC Executed'); cr. 
+     usedMemory := System _tempObjSpacePercentUsedLastMark.
+     Transcript show: ('Used Memory: ', usedMemory printString); cr. 
+     (usedMemory < commitThreshold)
+     ifTrue: [System enableAlmostOutOfMemoryError]"we are cool the memory droped below threshold"
+     ifFalse: [(ObjectLogEntry trace: 'AlmostOutOfMemory' object: (System _vmInstanceCounts: 3)) addToLog.
+               System commitTransaction].
+     ex resume.
+   ].
    SessionTemps current at: #'AlmostOutOfMemoryStaticException' put: handler.
    System signalAlmostOutOfMemoryThreshold: commitThreshold.
-
-[OrbeonLayerAppLinuxScripts startAllScript]
-  on: AlmostOutOfMemory enable
-  do: [:ex | ex error: ex description]. 
+  OrbeonLayerAppLinuxScripts startAllScript. 
 %
 exit
 EOF
