3890bffb3a857da2a577d44dda3c98c059a83713
brunobuzzi <bruno.brasesco@gmail.com>
2021-05-04 15:40:54 -0400

    update 02 for task 2) in #419

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -33,45 +33,43 @@ source ./setGs.sh -s $STONE -v $GS_VERSION
 info "Finish: Set Session Variables"
 
 info "Start STEP 1: Stop STONE ${STONE}"
+stopStone ${STONE}
 info "Finish STEP 1: Stop STONE ${STONE}"
 
 info "Start STEP 2: Move old or corrupted EXTENTS for further analysis"
+mkdir $GS_EXTENTS/deleted-extents
+cp $GS_EXTENTS/*.* $GS_EXTENTS/deleted-extents
 info "Finish STEP 2: Move old or corrupted EXTENTS for further analysis"
 
 info "Start STEP 3: Delete current extent from EXTENTS location"
+rm -Rf $GS_EXTENTS/*.*
 info "Finish STEP 3: Delete current extent from EXTENTS location"
 
 info "Start STEP 4: Copy clean EXTENTS to EXTENTS location"
+$GEMSTONE/bin/copydbf $GS_CLEAN_EXTENT $GS_EXTENTS/extent0.dbf
+chmod 600 $GS_EXTENTS/extent0.dbf
 info "Finish STEP 4: Copy clean EXTENTS to EXTENTS location"
 
 info "Start STEP 5: Check disk space for TRAN_LOGS"
 info "Finish STEP 5: Check disk space for TRAN_LOGS"
 
 info "Start STEP 6: Configure the EXTENTS for pre-grow"
+#DBF_EXTENT_SIZES = 3G;
+#DBF_PRE_GROW = TRUE;
 info "Finish STEP 6: Configure the EXTENTS for pre-grow"
 
 info "Start STEP 7: Start STONE ${STONE} in RESTORE mode"
+startStone -R ${STONE}
 info "Finish STEP 7: Start STONE ${STONE} in RESTORE mode"
 
-info "Start STEP 8: Log in to GemStone as DataCurator or SystemUser using linked Topaz (topaz -l)"
-info "Finish STEP 8: Log in to GemStone as DataCurator or SystemUser using linked Topaz (topaz -l)"
-
-info "Start STEP 9: Restore the most recent full backup to the new repository"
-info "Finish STEP 9: Restore the most recent full backup to the new repository"
-
-info "Start STEP 10: Restore TRAN_LOGS"
-info "Finish STEP 10: Restore TRAN_LOGS"
-
-
+info "Start STEP 8 & 9: Log in to GemStone as DataCurator or SystemUser using linked Topaz (topaz -l) and Restore the most recent full backup to the new repository"
 GS_USER=DataCurator
 PWD=`./getGsPwd.sh -u $GS_USER`
-$GS_HOME/bin/startTopaz $STONE -il <<EOF >>backup.log
+$GS_HOME/bin/startTopaz $STONE -il <<EOF >>$GS_LOGS/restore.log
 set user $GS_USER password $PWD gemstone $STONE
 login
 exec 
-  System abort.
-  SystemRepository fullBackupTo: '$GS_BACKUPS/$FILE'.
-  System commit.
+  SystemRepository restoreFromBackup: '$FILE'.
 %
 logout
 quit
@@ -82,5 +80,10 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+info "Finish STEP 8 & 9: Log in to GemStone as DataCurator or SystemUser using linked Topaz (topaz -l) and Restore the most recent full backup to the new repository"
 
-info "Finish: Backup STONE ${STONE_NAME} VERSION ${GS_VERSION}"
+
+info "Start STEP 10: Restore TRAN_LOGS"
+info "Finish STEP 10: Restore TRAN_LOGS"
+
+
